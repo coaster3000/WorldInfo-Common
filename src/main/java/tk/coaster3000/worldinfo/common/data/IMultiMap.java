0c9d8ef77fcf2f9ced11abd759d8d7fd8e3e0963
Christopher Krier <ckrier.3000@gmail.com>
2014-11-05 23:46:13 -0500

    Add missing license headers.

--- src/main/java/tk/coaster3000/worldinfo/common/data/IMultiMap.java
+++ src/main/java/tk/coaster3000/worldinfo/common/data/IMultiMap.java
@@ -1,3 +1,27 @@
+/**
+ * This file is part of WorldInfo, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) WorldInfo
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package tk.coaster3000.worldinfo.common.data;
 
 import java.util.Collection;
@@ -18,6 +42,9 @@ public interface IMultiMap<M extends Map<String, Object>> {
 	<T> T get(String path, Class<T> type);
 	<T> T get(String path, T defValue);
 
+	IMultiMap<M> getNode(String path);
+	IMultiMap<M> createNode(String path);
+
 	boolean set(String path, Object value);
 
 	Collection<String> getKeys(boolean recursive);
@@ -28,4 +55,7 @@ public interface IMultiMap<M extends Map<String, Object>> {
 
 	Map<String, Object> getData();
 	Map<String, Object> getFlatData();
+
+	void clear();
+	void deepClear();
 }
